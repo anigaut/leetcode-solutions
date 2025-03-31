f5d65245108235fb3750b9eee88bfa5793f6694c
anirudh <anirudhgr23@gmail.com>
2025-03-31 18:12:25 +0530

    Invert binary tree, max depth of binary tree

---
+++ resources.md
@@ -0,0 +1,2 @@
+# Trees
+- Greg Hogg: https://www.youtube.com/watch?v=EPwWrs8OtfI
---
+++ trees/invert_binary_tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+'''
+- Can be done recursively or iteratively.
+- If done iteratively, will need to use DFS or BFS and will require a deque.
+'''
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
